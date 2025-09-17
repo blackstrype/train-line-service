53a7b6829300468cab12dfe23e1fd8f32bee0147
blackstrype <blackstrype@gmail.com>
2025-09-17 17:58:42 +0200

    feat: Lab 2 complete - containerization and dynamic configuration

--- src/main/java/com/example/StatusResource.java
+++ src/main/java/com/example/StatusResource.java
@@ -1,16 +1,22 @@
 package com.example;
 
+import io.quarkus.logging.Log;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/status")
 public class StatusResource {
 
+    @ConfigProperty(name = "train-line-name")
+    String trainLineName;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String status() {
+        Log.info("Status check successful for " + trainLineName);
         return "Operational";
     }
 }
