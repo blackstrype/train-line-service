db3a093aee54eaeae660d715a66903f4226ce6fe
blackstrype <blackstrype@gmail.com>
2025-10-05 19:52:58 +0200

    feat: Lab 6 in progress - Testing and Implementing CircuitBreaker.

--- src/main/java/com/example/StationService.java
+++ src/main/java/com/example/StationService.java
@@ -6,6 +6,7 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
 import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.annotation.RegisterClientHeaders;
@@ -29,5 +30,11 @@ public interface StationService {
     @Produces(MediaType.APPLICATION_JSON)
     @Timeout(getStationTimeout)
     @Retry(maxRetries = getStationMaxRetries, maxDuration = 5000, delay = 1000)
+    @CircuitBreaker(
+            requestVolumeThreshold = 10, // Consider the last n requests
+            failureRatio = 0.60,         // If 60% fail...
+            delay = 10000,                // ...open the circuit for a while
+            successThreshold = 2         // Close circuit after 2 consecutive successes
+    )
     Station getStationById(@PathParam("id") String id);
 }
